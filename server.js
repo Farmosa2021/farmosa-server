724194cf07c4329ecddf0df61a9828c5ea12219e
Chr1sInTheHouse <bpploabc@gmail.com>
2021-06-03 18:23:51 +0800

    we have database now

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use('/fruits', fruits);
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-app.listen(PORT, HOST);
+app.listen(PORT, HOST, function(){
+    predict_table.update();
+    price_query_table.update();
+});
 console.log(`Running on http://${HOST}:${PORT}`);
-
-predict_table.update();
-price_query_table.update();
--- services/update_predict_table.js
+++ services/update_predict_table.js
@@ -3,37 +3,35 @@
 const db = require('./db');
 
 async function update() {
-    await db.query("DROP TABLE IF EXISTS predict_table").then(() => {
-        // Query to create table "sample"
-        var createStatament = "CREATE TABLE predict_table ( \
-            時間 DATE not null, \
-            洋蔥 FLOAT, \
-            青香蕉 FLOAT, \
-            鳳梨 FLOAT, \
-            蒜頭 FLOAT, \
-            青梅 FLOAT, \
-            木瓜 FLOAT, \
-            柳橙 FLOAT, \
-            椪柑 FLOAT, \
-            蓮霧 FLOAT, \
-            棗子 FLOAT, \
-            新興梨 FLOAT, \
-            檸檬 FLOAT, \
-            芒果 FLOAT, \
-            豐水梨 FLOAT, \
-            番石榴 FLOAT, \
-            文旦 FLOAT, \
-            甜柿 FLOAT, \
-            桶柑 FLOAT, \
-            紅龍果 FLOAT, \
-            甘藍 FLOAT, \
-            結球白菜 FLOAT, \
-            茂谷柑 FLOAT, \
-            西瓜 FLOAT, \
-            PRIMARY KEY(時間) \
-            )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
-        db.query(createStatament);
-    });
+    await db.query("DROP TABLE IF EXISTS predict_table");
+    var createStatament = "CREATE TABLE predict_table ( \
+        時間 DATE not null, \
+        洋蔥 FLOAT, \
+        青香蕉 FLOAT, \
+        鳳梨 FLOAT, \
+        蒜頭 FLOAT, \
+        青梅 FLOAT, \
+        木瓜 FLOAT, \
+        柳橙 FLOAT, \
+        椪柑 FLOAT, \
+        蓮霧 FLOAT, \
+        棗子 FLOAT, \
+        新興梨 FLOAT, \
+        檸檬 FLOAT, \
+        芒果 FLOAT, \
+        豐水梨 FLOAT, \
+        番石榴 FLOAT, \
+        文旦 FLOAT, \
+        甜柿 FLOAT, \
+        桶柑 FLOAT, \
+        紅龍果 FLOAT, \
+        甘藍 FLOAT, \
+        結球白菜 FLOAT, \
+        茂谷柑 FLOAT, \
+        西瓜 FLOAT, \
+        PRIMARY KEY(時間) \
+        )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+    await db.query(createStatament);
 
     // Creating table "predict_table"
     // var loadStatement = "load data local infile '../data/predict_table.csv' \
