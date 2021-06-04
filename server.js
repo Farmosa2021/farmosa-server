0d2d21a8e89ddc17cecab46e2e17ac2bae65b698
WaterSo0910 <jacky950141@gmail.com>
2021-06-04 08:38:16 -0700

    update basic fruit table(have BUG)

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const fruit_price = require('./routes/fruit_price');
 
 const predict_table = require('./services/update_predict_table');
 const price_query_table = require('./services/update_price_query_table');
+const build_fruit_db = require('./services/build_fruit_db');
 
 const predict_price = require('./routes/predict_price');
 
@@ -25,5 +26,6 @@ const HOST = '0.0.0.0';
 app.listen(PORT, HOST, function(){
     predict_table.update();
     price_query_table.update();
+    build_fruit_db.imageCrawler();
 });
 console.log(`Running on http://${HOST}:${PORT}`);
