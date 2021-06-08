f15718abca86f21a3fffe5f549a4b4b72eb391c3
YuHo-chang <changhoward.cs07@nctu.edu.tw>
2021-06-09 01:08:12 +0800

    fruit_db

---
+++ services/fruit_db.js
@@ -0,0 +1,126 @@
+const db = require("./db");
+
+async function search_realtime_by_sub(fruit) {
+    var statement = "SELECT * FROM price_query where item LIKE '%" + fruit + "%'";
+    try{
+        const data = await db.query(statement);
+        if(data.length==0){
+            return { result: "error" };
+        }
+        const response = { result: "success" };
+        return {
+            data,
+            response,
+        };
+    }catch(err){
+        console.log(err)
+        return { result: "error" };
+    }
+}
+
+async function search_realtime_by_fullname(fruit) {
+    var statement = "SELECT * FROM price_query where item = (?)";
+    try{
+        const data = await db.query(statement, fruit);
+        if(data.length==0){
+            return { result: "error" };
+        }
+        const response = { result: "success" };
+        return {
+            data,
+            response,
+        };
+    }catch(err){
+        console.log(err)
+        return { result: "error" };
+    }
+}
+
+async function search_history_by_sub(fruit) {
+    var statement = "SHOW COLUMNS FROM predict_table LIKE '%" + fruit + "%'";
+    name_list = await db.query(statement);
+    var columns = "DATE";
+    for (i = 0; i<  len(name_list); i++){
+        columns += (", " +name_list[i].field)
+    }
+    columns = columns.slice(0, -2)  
+    try{
+        statement = "SELECT" + columns + " FROM predict_table"
+        const data = await db.query(statement);
+        if(data.length==0){
+            return { result: "error" };
+        }
+        const response = { result: "success" };
+        return {
+            data,
+            response,
+        };
+    }catch(err){
+        console.log(err)
+        return { result: "error" };
+    }
+}
+
+async function search_history_by_fullname(fruit) {
+    var statement = "SELECT DATE, " + fruit + " FROM predict_table";
+    try{
+        const data = await db.query(statement, fruit);
+        if(data.length==0){
+            return { result: "error" };
+        }
+        const response = { result: "success" };
+        return {
+            data,
+            response,
+        };
+    }catch(err){
+        console.log(err)
+        return { result: "error" };
+    }
+}
+
+async function search_fruit() {
+    var statement = "SELECT * FROM fruit";
+    try{
+        const data = await db.query(statement, fruit);
+        if(data.length==0){
+            return { result: "error" };
+        }
+        const response = { result: "success" };
+        return {
+            data,
+            response,
+        };
+    }catch(err){
+        console.log(err)
+        return { result: "error" };
+    }
+}
+
+async function search_image_by_fruit(fruit) {
+    var statement = "SELECT image FROM fruit WHERE name = (?)";
+    try{
+        const data = await db.query(statement, fruit);
+        if(data.length==0){
+            return { result: "error" };
+        }
+        const response = { result: "success" };
+        return {
+            data,
+            response,
+        };
+    }catch(err){
+        console.log(err)
+        return { result: "error" };
+    }
+}
+
+module.exports = {
+    // add_some_data,
+    search_realtime_by_sub,
+    search_realtime_by_fullname,
+    search_history_by_fullname,
+    search_history_by_sub,
+    search_fruit,
+    search_image_by_fruit
+};
